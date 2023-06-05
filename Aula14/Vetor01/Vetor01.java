9b577035af67d286b4c51746f8761720db654db2
Marconny Marques <130397974+Marconny-Marques@users.noreply.github.com>
2023-06-05 16:59:17 -0300

    \n quebra a linha

--- Aula14/Vetor01/Vetor01.java
+++ Aula14/Vetor01/Vetor01.java
@@ -4,7 +4,7 @@ public class Vetor01 {
     public static void main(String[] args) {
         int n[] = {3,2,8,7,5,4};
         for(int c=0; c<=n.length-1; c++) {
-            System.out.print("Na posição " + c + " temos o valor " + n[c]);
+            System.out.print("Na posição " + c + " temos o valor \n"  + n[c]);
         }
     }
 }
