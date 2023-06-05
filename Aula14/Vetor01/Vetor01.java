cd0358d1ab89048e0c0132b3d2494ada7c8bffaf
Marconny Marques <130397974+Marconny-Marques@users.noreply.github.com>
2023-06-05 15:52:46 -0300

    Vetores java

---
+++ Aula14/Vetor01/Vetor01.java
@@ -0,0 +1,10 @@
+package Aula14.Vetor01;
+
+public class Vetor01 {
+    public static void main(String[] args) {
+        int n[] = {3,2,8,7,5,4};
+        for(int c=0; c<=n.length-1; c++) {
+            System.out.print("Na posição " + c + " temos o valor " + n[c]);
+        }
+    }
+}
